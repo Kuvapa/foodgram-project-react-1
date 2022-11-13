c80db144a24fc8cdb5d53d24ee6bb0bfbb465097
UnRainbow <ranmadm@gmail.com>
2022-11-13 16:27:02 +0300

    permissions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedUrReadOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
