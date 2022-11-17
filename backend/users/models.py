2d45da05cc3a8c3c559c6136fb07cf40ee4f0174
Anna Pervukhina <101391679+pervukhina-anna@users.noreply.github.com>
2022-11-17 23:24:55 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
     )
     email = models.EmailField(
         verbose_name='Адрес электронной почты',
-        help_text='Укажите адоес электронной почты',
+        help_text='Укажите адрес электронной почты',
         max_length=254,
         unique=True,
         error_messages={'unique': 'Этот email уже зарегистрирован!'}
